70129985b9a4f59194c02018df4283535c78b1dc
BugHunter-X <askzenai@gmail.com>
2025-01-05 14:26:05 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // Simulate a long-running task
+  let count = 0;
+  while (count < 1000000000) {
+    count++;
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
